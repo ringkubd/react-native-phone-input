7dc7cc6d534137061bf2b84f1240e54733aa073d
Anwar Jahid <ajr.jahid@gmail.com>
2023-12-15 20:26:34 +0600

    flag removed

--- bin/build.sh
+++ bin/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 rm -rf ./dist && \
-mkdir dist && mkdir dist/resources && mkdir dist/resources/flags &&  \
-cp -r ./src/resources/flags/images ./dist/resources/flags && \
-mkdir dist/typings && cp -r ./src/typings ./dist \
+mkdir dist && mkdir dist/resources \
+&& mkdir dist/typings && cp -r ./src/typings ./dist \
 && tsc
